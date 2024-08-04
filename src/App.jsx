e22433142ea673930d766bdc77c1d5f1678db999
Adam Wheatley <wheatley.adam@me.com>
2024-08-04 14:30:13 -0500

    add projects sidebar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <div className="h-screen my-8">
+      <ProjectsSidebar />
+    </div>
+  );
+}
+
+export default App;
