5eec9a3097e44a35a15704d37a76aa4fda95615b
Adam Wheatley <wheatley.adam@me.com>
2024-08-12 20:29:58 -0500

    add screen for no project selected

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,11 @@
+import noProjectImage from '../assets/no-projects.png';
+import Button from './Button';
+
+export default function NoProjectSelected() {
+  return <div className="mt-24 text-center w-2/3">
+    <img className="w-16 h-16 object-contain mx-auto" src={noProjectImage} alt="digital art of a clipboard and pen" />
+    <h2 className="text-xl text-stone-500 font-bold my-4">No project selected</h2>
+    <p className="text-stone-400 mb-12">Select a project or get started with a new one</p>
+    <Button>Create new project</Button>
+  </div>
+}
